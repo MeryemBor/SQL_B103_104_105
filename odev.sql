996c7e9cb9b98cf168698ceec3939284cfe0090f
Meryem <munirebor@gmail.com>
2022-12-08 23:23:07 +0000

    08/11/22

--- odev.sql
+++ odev.sql
@@ -1,10 +1,11 @@
---ODEV
--- Ankara’da calisani olan markalarin marka id'lerini ve calisan sayilarini listeleyiniz
+
+
+--ÖDEV
+-- Ankara’da calisani olan markalarin marka id'lerini ve 
+-- calisan sayilarini listeleyiniz
+
+SELECT marka_id as ankaradacalisanid,calisan_sayisi FROM markalar
+WHERE marka_isim IN (SELECT isyeri FROM calisanlar2 WHERE sehir='Ankara');
+
 select * from markalar
 select * from calisanlar2
-
-select marka_id as ankaradacalisanid,calisan_sayisi from markalar
-WHERE marka_isim IN (SELECT isyeri FROM calisanlar2 where seZhir ='Ankara');
-
-
-
